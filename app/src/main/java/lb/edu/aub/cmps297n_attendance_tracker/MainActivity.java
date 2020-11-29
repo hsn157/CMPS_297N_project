5ac0dbe939e8b76c66061816389b629d51902748
Hassan Abbass <haa157@mail.aub.edu>
2020-11-29 15:07:57 +0200

    First commit

---
+++ app/src/main/java/lb/edu/aub/cmps297n_attendance_tracker/MainActivity.java
@@ -0,0 +1,14 @@
+package lb.edu.aub.cmps297n_attendance_tracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
